02ef768c0aaa249f7f1820699aa3a6f3a165280d
hdu406 <ciyunruoshui@163.com>
2018-11-12 20:11:03 +0800

    currentThread与this的不同

---
+++ Thread/Test.java
@@ -0,0 +1,11 @@
+package Java_Design_Moduls7;
+
+public class Test {
+    public static void main(String[] args){
+
+        MyThread myThread = new MyThread();
+        myThread.setName("A");
+        myThread.start();
+
+    }
+}
